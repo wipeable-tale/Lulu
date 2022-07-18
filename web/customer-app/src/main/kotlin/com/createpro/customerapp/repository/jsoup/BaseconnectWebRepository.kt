10edb721326cdc299bcd684dddbb2dbc520d0027
堀川翔 <horikawashou@horikawashounoMacBook-Pro.local>
2022-07-19 01:42:09 +0900

    teporary implemented web repository for Baseconnect

--- web/customer-app/src/main/kotlin/com/createpro/customerapp/repository/jsoup/BaseconnectWebRepository.kt
+++ web/customer-app/src/main/kotlin/com/createpro/customerapp/repository/jsoup/BaseconnectWebRepository.kt
@@ -2,8 +2,9 @@ package com.createpro.customerapp.repository.jsoup
 
 import com.createpro.customerapp.model.BASECONNECT
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 import org.springframework.stereotype.Service
+
 
 @Service
 class BaseconnectWebRepository {
@@ -14,28 +15,47 @@ class BaseconnectWebRepository {
         val countInfo = doc.getElementsByClass(BASECONNECT.COUNTS.value)?.text()
         val count = countInfo.replace()
 
-        val details = doc.getDetails()
         return Integer.parseInt(count)
     }
 
     fun fetchCompanyDetail(): Unit {
         val doc = Jsoup.connect(BASECONNECT.URL.value).get()
         val companyElements = doc.getElementsByClass("searches__result__list")
+        //テスト用
+//        val element = companyElements.first()!!
+
         for (element in companyElements) {
-            println("---------")
-            val detailUri =
-                element.getElementsByClass("searches__result__list__header__title").first()?.getElementsByTag("a")
-                    ?.first()?.attr("href").orEmpty()
-            val companyName =
-                element.getElementsByClass("searches__result__list__header__title").first()?.getElementsByTag("a")
-                    ?.first()?.text()
             val category =
                 element.getElementsByClass("searches__tag searches__tag--listed")?.first()?.getElementsByTag("a")
                     ?.text()
+            val detailUri =
+                element.getElementsByClass("searches__result__list__header__title").first()?.getElementsByTag("a")
+                    ?.first()?.attr("href").orEmpty()
+            val detailDoc = Jsoup.connect("${BASECONNECT.DETAIL_URL.value}${detailUri}").get()
 
+            val companyName = detailDoc.getElementsByClass("node__header__text__title").text()
+            val companyBaseInfo =
+                detailDoc.getElementsByClass("nodeTable--simple nodeTable--simple__twoColumn nodeTable--simple__twoColumn_side")
+                    .first()!!
+                    .getElementsByTag("dl")
+            val establish = fetchItem(companyBaseInfo, "設立年月")
+            val foundation = fetchItem(companyBaseInfo, "資本金")
+            val benefit = fetchItem(companyBaseInfo, "売上高")
+            val numberOfEmployee = fetchItem(companyBaseInfo, "従業員数")
+            val newHired = fetchItem(companyBaseInfo, "新卒採用人数")
+            val officesNumber = fetchItem(companyBaseInfo, "事業所数")
+
+            println("-------")
             println(companyName)
-            println(detailUri)
             println(category)
+            println(establish)
+            println(foundation)
+            println(benefit)
+            println(numberOfEmployee)
+            println(newHired)
+            println(officesNumber)
+
+
         }
 
 
@@ -52,15 +72,33 @@ class BaseconnectWebRepository {
         return value
     }
 
-    private fun Document.getDetails(): List<String> {
-        val elements = getElementsByClass("searches__result__list__header__title")
-
-        var data = arrayListOf<String>()
-        for (element in elements) {
-            val url = element.getElementsByTag("a").first()?.attr("href").orEmpty()
-            data.add(url)
+    private fun fetchItem(baseInfos: Elements, target: String): String {
+        var value = ""
+        for (base in baseInfos) {
+            val key = base.getElementsByTag("dt").text()
+            if (key == target)
+                when (key) {
+                    "設立年月" -> {
+                        value = base.getElementsByTag("dd").text()
+                    }
+                    "資本金" -> {
+                        value = base.getElementsByTag("dd").text()
+                    }
+                    "売上高" -> {
+                        value = base.getElementsByTag("dd").text()
+                    }
+                    "従業員数" -> {
+                        value = base.getElementsByTag("dd").text()
+                    }
+                    "新卒採用人数" -> {
+                        value = base.getElementsByTag("dd").text()
+                    }
+                    "事業所数" -> {
+                        value = base.getElementsByTag("dd").text()
+                    }
+                }
         }
-        return data
+        return value
     }
 
 }
